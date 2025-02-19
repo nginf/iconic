4f4ea78c1cf91cadde6419d32348cd45ded72c73
luka onik <luka.onikadze@toptal.com>
2025-02-19 23:09:53 +0400

    update icon

--- projects/md/src/base-icon.ts
+++ projects/md/src/base-icon.ts
@@ -1,5 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, effect } from '@angular/core';
 import { CommonIcon } from '../../common/src/public-api';
 
 @Directive()
-export class BaseIcon extends CommonIcon {}
+export class BaseIcon extends CommonIcon {
+  constructor() {
+    super();
+
+    effect(() => {
+      const icon = this.icon;
+      icon.setAttribute('fill', 'currentColor');
+    });
+  }
+}
